601869df6f2dc026a7480aca26205842df86b356
Roan Jones <honestly2978@gmail.com>
2024-07-23 20:51:54 +0100

    console based pomodoro timer

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 import time
-
-# time in minutes
-total_time = 60
-study_time = 10
-break_time = 5
-
-total_interval = study_time + break_time #15
-
-count = total_time
-
-for i in range(study_time+1):
-
-    print("Study time: " + str(total_time-i))
-    count -= 1
-    #time.sleep(1)
+import threading
 
 
-print(count)
+def start_timer(duration):
+    print(f"Starting timer for {duration // 60} minutes.")
+    countdown(duration)
+
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)  # div mod gives you the quotient, remainder 
+
+        #: = start of the format code.
+        #0 = the number should be zero-padded 
+        #2 = the number should be at least 2 digits wide.
+        #d = "decimal integer".
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        
+        print(timer, end="\r") # overwrites current line with printed value rather than go to the next line
+        time.sleep(1)
+        t -= 1
+    print('Timer finished!')
+
+
+def pomodoro_cycle(work_duration, short_break, long_break, cycles):
+    for i in range(cycles):
+        start_timer(work_duration)
+        print("Take a short break.")
+        start_timer(short_break)
+
+    print("Time for a long break!")
+    start_timer(long_break)
+
+
+def main():
+    work_duration = 25 #* 60
+    short_break = 5 #* 60
+    long_break = 15 #* 60
+    cycles = 4
+
+    pomodoro_cycle(work_duration, short_break, long_break, cycles)
+
+
+if __name__ == "__main__":
+    main()
